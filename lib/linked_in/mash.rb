581afd2c8f053a0ca3a9adbed0d340a243444e2e
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2022-10-03 19:56:49 +0200

    Fix: Remove useless hack.

--- lib/linked_in/mash.rb
+++ lib/linked_in/mash.rb
@@ -35,34 +35,5 @@ module LinkedIn
     def contains_date_fields?
       self.year? && self.month? && self.day?
     end
-
-    # overload the convert_key mash method so that the LinkedIn
-    # keys are made a little more ruby-ish
-    def convert_key(key)
-      case key.to_s
-      when '_key'
-        'id'
-      when '_total'
-        'total'
-      when 'values'
-        'all'
-      when 'numResults'
-        'total_results'
-      else
-        underscore(key)
-      end
-    end
-
-    # borrowed from ActiveSupport
-    # no need require an entire lib when we only need one method
-    def underscore(camel_cased_word)
-      word = camel_cased_word.to_s.dup
-      word.gsub!(/::/, '/')
-      word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
-      word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
-      word.tr!("-", "_")
-      word.downcase!
-      word
-    end
   end
 end
