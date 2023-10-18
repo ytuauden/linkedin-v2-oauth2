89b9c1231d2f2e9c56806dd7c460d051ebe922e4
Yohan Tuauden <ytuauden@gmail.com>
2023-10-18 15:48:28 +0200

    Improvement: Implement get single image endpoint. RD-24577

--- lib/linked_in/images.rb
+++ lib/linked_in/images.rb
@@ -4,6 +4,12 @@ module LinkedIn
   # @see https://docs.microsoft.com/en-us/linkedin/marketing/integrations/community-management/shares/images-api?view=li-lms-2022-07
   class Images < APIResource
 
+    def image(options = {})
+      path = "#{images_path(options)}/#{CGI.escape(options.delete(:image_urn))}"
+      options = set_restli_protocol_version_header(options)
+      get(path, options)
+    end
+
     def images(options = {})
       path = images_path(options)
       options['ids'] = "List(#{options.delete(:images_urn).map { |urn| CGI.escape(urn) }.join(',')})"
